3ad33b8745cc6442494bc093f8861f0d3da42248
Psancegu <psancegu9@alumnes.ub.edu>
2025-01-29 12:59:07 +0100

    VanyaAndFences resolt

---
+++ 800rated/VanyaAndFence/VanyaAndFence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class VanyaAndFence{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+
+		int n, h, w = 0;
+
+		n = sc.nextInt();
+		h = sc.nextInt();
+
+		for (int i = 0; i < n; i++){
+			if(sc.nextInt() > h){
+				w += 2;
+			}
+			else{
+				w += 1;
+			}
+		}
+		System.out.println(w);
+	}
+}
