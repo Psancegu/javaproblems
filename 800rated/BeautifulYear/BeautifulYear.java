243fac58cd9820365a61cca5001dff02e0d12539
Psancegu <psancegu9@alumnes.ub.edu>
2025-02-04 14:00:54 +0100

    Beautiful Year Semidone

---
+++ 800rated/BeautifulYear/BeautifulYear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BeautifulYear{
+	public static void main( String [] args){
+		Scanner sc = new Scanner(System.in);
+
+		boolean notDistinct = true;
+		String startingYear = sc.next();
+		
+
+		while (notDistinct){
+			if (
+		}
+	}
+}
